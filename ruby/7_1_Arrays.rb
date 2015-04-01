f512bd5f9bd5bb4bad19f3bfcf496dbdf55b49f2
Diego Camacho <hackvan@gmail.com>
2015-03-31 21:14:17 -0500

    Removing old files renamed.

--- ruby/7_1_Arrays.rb
+++
@@ -1,9 +0,0 @@
-my_array = [1,2,3,4]
-puts my_array[0]
-puts my_array[1]
-puts my_array[2]
-puts my_array[3]
-
-multi_d_array = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
-
-multi_d_array.each { |x| puts "#{x}\n" }
--- ruby/9_2_Splat_Arguments.rb
+++
@@ -1,5 +0,0 @@
-def what_up(greeting, *bros)
-  bros.each { |bro| puts "#{greeting}, #{bro}!" }
-end
- 
-what_up("What up", "Justin", "Ben", "Kevin Sorbo")
